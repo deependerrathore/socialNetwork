2444b8c961a86c69e7f48873180a266f6bd15950
deependerrathore <deepenrathore28@gmail.com>
2017-12-03 18:39:12 +0530

    setting login token and cokies

--- login.php
+++ login.php
@@ -6,10 +6,15 @@
 		$password = $_POST['password'];
 
 		if(DB::query('SELECT username FROM users WHERE username = :username' , array(':username'=>$username))){
-			echo $password;
-			echo DB::query('SELECT password FROM users WHERE username = :username',array(':username' => $username))[0]['password'];
+			
 			if(password_verify($password,DB::query('SELECT password FROM users WHERE username = :username',array(':username' => $username))[0]['password'])){
 				echo 'User logged in';
+				$cstring = TRUE;
+				$token = bin2hex(openssl_random_pseudo_bytes(64,$cstring));
+				
+				setcookie("SNID",$token,time() + 60 * 60 * 24 * 7 , '/',NULL,NULL, TRUE);
+				$user_id = DB::query('SELECT id FROM users WHERE username = :username',array(':username'=>$username))[0]['id'];
+				DB::query('INSERT into login_tokens VALUES(null, :token ,:user_id)',array(':token'=>sha1($token),':user_id'=>$user_id));
 			}else{
 				echo 'Incorrect password!';
 			}
