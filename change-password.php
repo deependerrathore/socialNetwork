808b4aeb2442741b7b04b3dece5f2f8fb77a5a22
deependerrathore <deepenrathore28@gmail.com>
2017-12-09 03:05:18 +0530

    forgot password

--- change-password.php
+++ change-password.php
@@ -1,7 +1,7 @@
 <?php 
 	include('classes/DB.php');
 	include ('classes/Login.php');
-
+	$tokenIsValid = FALSE;	
 	if (Login::isLoggedIn()) {
 		if (isset($_POST['changepassword'])) {
 			$oldpassword = $_POST['oldpassword'];
@@ -25,14 +25,53 @@
 			}
 		}
 	}else{
-		die('User not logged in');
+
+		if (isset($_GET['token'])) {
+			
+			
+			
+			$token = $_GET['token'];
+
+			if (DB::query('SELECT user_id FROM password_tokens where token =:token',array(':token'=>sha1($token)))) {
+			
+				$userid = DB::query('SELECT user_id FROM password_tokens where token =:token',array(':token'=>sha1($token)))[0]['user_id'];
+
+				$tokenIsValid = TRUE;
+				if (isset($_POST['changepassword'])) {
+				
+				$newpassword = $_POST['newpassword'];
+				$newpasswordrepeat = $_POST['newpasswordrepeat'];
+
+				
+				
+					if ($newpassword == $newpasswordrepeat) {
+						if (strlen($newpassword) >=6 && strlen($newpassword) <=60) {
+
+
+							DB::query('UPDATE users SET password = :newpassword WHERE id=:userid',array(':newpassword'=>password_hash($newpassword,PASSWORD_BCRYPT),':userid'=>$userid));
+							echo 'Password changed successfully!';
+							DB::query('DELETE FROM password_tokens WHERE user_id= :userid',array(':userid'=>$userid)); 
+						}
+					}else{
+						echo 'password not matching';
+					}
+				
+			}
+
+			}else{
+				die('Invalid token!');
+			}
+		}else{
+			die('User not logged in');	
+		}		
+		
 	}
 
  ?>
 
  <h1>Change your password</h1>
- <form action="change-password.php" method="post">
- 	<input type="password" name="oldpassword" placeholder="Current password..."><p />
+ <form action="<?php if(!$tokenIsValid){echo 'change-password.php';}else{ echo 'change-password.php?token='.$token.'';} ?>" method="post">
+ 	<?php if(!$tokenIsValid){ echo '<input type="password" name="oldpassword" placeholder="Current password..."><p />';}  ?>
  	<input type="password" name="newpassword" placeholder="New password..."><p />
  	<input type="password" name="newpasswordrepeat" placeholder="Repeat new password..."><p />
  	<input type="submit" name="changepassword" value="Change Password" >
