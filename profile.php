10bb25f30f0451a355391a98cb7111ff3bc9a996
deependerrathore <deepenrathore28@gmail.com>
2017-12-10 00:15:03 +0530

    Unfollow users

--- profile.php
+++ profile.php
@@ -3,22 +3,44 @@
 	include ('classes/Login.php');
 
 	$username = "";
+	$isFollowing = FALSE;
+
 	if(isset($_GET['username'])){
 		if (DB::query('SELECT username from users WHERE username = :username',array(':username'=>$_GET['username']))) {
 			$username = DB::query('SELECT username FROM users WHERE username=:username',array(':username'=>$_GET['username']))[0]['username'];
 
+			$userid = DB::query('SELECT id FROM users WHERE username = :username',array(':username'=>$_GET['username']))[0]['id'];
+
+			$followerid = Login::isLoggedIn();
+
+			
+			
 			if (isset($_POST['follow'])) {
+				if ($userid != $followerid) {
+					if (!DB::query('SELECT follower_id FROM followers WHERE user_id = :userid',array(':userid'=>$userid))) {
+						DB::query('INSERT INTO followers values (null,:userid,:followerid)',array(':userid'=>$userid,':followerid'=>$followerid));
+					}else{
+						echo 'Already following!';
+					}
+					$isFollowing = TRUE;
+				}
 				
-				$userid = DB::query('SELECT id FROM users WHERE username = :username',array(':username'=>$_GET['username']))[0]['id'];
-
-				$followerid = Login::isLoggedIn();
-
-				if (!DB::query('SELECT follower_id FROM followers WHERE user_id = :userid',array(':userid'=>$userid))) {
-					DB::query('INSERT INTO followers values (null,:userid,:followerid)',array(':userid'=>$userid,':followerid'=>$followerid));
-				}else{
-					echo 'Already following!';
+			}
+			if (isset($_POST['unfollow'])) {
+				if ($userid != $followerid) {
+					if (DB::query('SELECT follower_id FROM followers WHERE user_id = :userid',array(':userid'=>$userid))) {
+						DB::query('DELETE FROM followers WHERE user_id =:userid AND follower_id =:followerid',array(':userid'=>$userid,':followerid'=>$followerid));
+					}
+					$isFollowing = FALSE;
 				}
 			}
+
+			if (DB::query('SELECT follower_id FROM followers WHERE user_id = :userid',array(':userid'=>$userid))) {
+				//echo 'Already following!';
+				$isFollowing = TRUE;
+
+			}
+			
 		}else{
 			die('User not found!');
 		}
@@ -27,5 +49,14 @@
  <h1><?php echo $username;?>'s Profile</h1>
 
  <form action="profile.php?username=<?php echo $username;?>" method="post">
- 	<input type="submit" name="follow" value="Follow">
+ 	<?php 
+ 	if ($userid != $followerid) {
+ 		if ($isFollowing) {
+ 			echo '<input type="submit" name="unfollow" value="UnFollow">';
+ 		}else{
+ 			echo '<input type="submit" name="follow" value="Follow">';
+ 		}	
+ 	}
+ 	
+ 	?>
  </form>
