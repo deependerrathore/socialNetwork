bd55a9255620576e1b3b818fc72bb197905c3259
deependerrathore <deepenrathore28@gmail.com>
2017-12-11 00:06:24 +0530

    Timeline start

--- index.php
+++ index.php
@@ -2,11 +2,21 @@
 	include('classes/DB.php');
 	include ('classes/Login.php');
 
+	$showTimeline = FALSE;
 	if (Login::isLoggedIn()) {
-		echo 'User logged in';
 		echo Login::isLoggedIn();
+		$showTimeline = TRUE;
 	}else{
 		echo 'User not logged in';
 	}
 
+	$followingposts = DB::query('SELECT posts.post,posts.likes,users.username FROM posts,followers,users
+	WHERE posts.user_id = followers.user_id
+	and posts.user_id = users.id
+	AND followers.follower_id = :userid
+	ORDER BY posts.likes DESC',array(':userid'=>Login::isLoggedIn()));
+
+	foreach ($followingposts as $post) {
+		echo $post['post'] . "~". $post['username'] ." " .$post['likes']." likes <hr />";
+	}
  ?>
