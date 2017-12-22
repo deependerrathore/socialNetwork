1b547c7fe98e5907a4bc2a112bbc41e3b1224bec
deependerrathore <deepenrathore28@gmail.com>
2017-12-22 21:31:57 +0530

    CSRF protection

--- send-message.php
+++ send-message.php
@@ -1,7 +1,13 @@
-<?php 
+<?php
+	session_start(); 
 	include('classes/DB.php');
 	include ('classes/Login.php');
-
+	$cstring = TRUE;
+	$token = bin2hex(openssl_random_pseudo_bytes(64,$cstring));
+	if (isset(!$_SESSION['token'])) {
+		$_SESSION['token'] = $token;	
+	}
+	
 	if (Login::isLoggedIn()) {
 		$userid = Login::isLoggedIn();
 	}else{
@@ -9,6 +15,14 @@
 	}
 
 	if (isset($_POST['send'])) {
+
+		if (!isset($_POST['nocsrf'])) {
+			die('Invalid token!');
+		}
+
+		if ($_POST['nocsrf'] != $_SESSION['token']) {
+			die('Invalid token!');
+		}
 		
 		if (strlen($_POST['body'] > '0' )) {
 			if (DB::query('SELECT id FROM users WHERE id= :receiver',array(':receiver'=>$_GET['receiver']))) {
@@ -23,6 +37,7 @@
 			echo 'Please type in a message!';
 		}
 		
+		session_destroy();
 		
 	}
 ?>
@@ -30,6 +45,7 @@
 <h1>Send a message</h1>
 <form action="send-message.php?receiver=<?php echo htmlspecialchars($_GET['receiver']) ?>" method="POST">
 	<textarea rows="10" cols="80" name="body"></textarea>
+	<input type="hidden" name="nocsrf" value="<?php echo $_SESSION['token']; ?>">
 	</br>
 	<input type="submit" name="send" value="Send Message">
 </form>
