4239773add9c220be0fd4f607fba2a479907d852
deependerrathore <deepenrathore28@gmail.com>
2018-01-10 22:45:45 +0530

    Designing the sign up page

--- api/index.php
+++ api/index.php
@@ -4,6 +4,52 @@
 
 	$db = new DB("127.0.0.1","SocialNetwork","root","admin");
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+		if ($_GET['url'] == 'users') {	
+			$postBody = file_get_contents("php://input");
+			$postBody = json_decode($postBody);
+			$username = $postBody->username;
+			$password = $postBody->password;
+			$email = $postBody->email;
+			if (!$db->query('SELECT username FROM users WHERE username = :username', array(':username'=>$username))) {
+				
+			if (strlen($username) >=3 && strlen($username) <=32) {
+				if (strlen($password) >=6 && strlen($password) <=60) {
+					if (preg_match('/^[a-zA-Z0-9_]+$/', $username)) {
+						if (filter_var($email,FILTER_VALIDATE_EMAIL)) {	
+							if (!$db->query('SELECT email FROM users WHERE email = :email',array(':email'=>$email))) {
+								$db->query('INSERT INTO users VALUES (null,:username , :password, :email,0,null)', array(':username'=>$username,':password'=>password_hash($password,PASSWORD_BCRYPT),':email'=>$email));
+								echo '{"Success":"User created!"}';
+								http_response_code(200);
+							}else{
+								echo '{"Error":"Email in use!"}';
+								http_response_code(409);
+							}		
+							
+						}else{
+							echo '{"Error":"Invalid email address!"}';
+							http_response_code(409);
+							
+						}
+						
+					}else{
+						echo '{"Error":"Invalid character in username!"}';
+						http_response_code(409);
+					}	
+				}else{
+					echo '{"Error":"Invalid length of password! must be between 6 to 60!"}';
+					http_response_code(409);
+									}
+			}else{
+				echo '{"Error":"Invalid length of username! must be between 3 to 32!"}';
+				http_response_code(409);
+			}
+			
+		}else{
+			echo '{"Error":"User already exist!"}';
+			http_response_code(409);
+		}
+		}
 		if ($_GET['url'] == 'auth') {
 			$postBody = file_get_contents("php://input");
 			$postBody = json_decode($postBody);
